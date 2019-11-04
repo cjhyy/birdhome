acfec52017714851ec16b87f97ef57c1afa17415
maki maki <chenjunhong54321@163.com>
2019-11-04 10:45:29 +0800

    头部修改

--- src/head/head.tsx
+++ src/head/head.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Menu from './menu'
+import Logo from './logo'
 
 const Header=()=>{
     return (
         <div>
+                       <Logo/>
            <Menu/>
+
         </div>
     )
 }
---
+++ src/head/logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './logo.css'
+
+const Logo=(    )=>{
+    return (
+        <div>
+            <p className='logo' >初燕坊logo</p>
+        </div>
+    )
+}
+
+export default Logo
